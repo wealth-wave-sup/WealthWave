31ac4cfef6a6a354bbbff342f37525c1a73a60d3
Taha <bit21it07@gmail.com>
2024-10-14 22:05:00 +0530

    main.dart edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wealthwave/screens/splash_screen/splash_screen.dart';
-import '/responsive_builder.dart';
-import 'package:wealthwave/screens/login_page/login_screen.dart';
 
 //firebase integration
 import 'package:firebase_core/firebase_core.dart';
@@ -23,10 +21,7 @@ class MyApp extends StatelessWidget {
 
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ResponsiveBuilder(
-          mobile: SplashScreen(),
-          tablet: SplashScreen(),
-          desktop: LoginScreen()),
+      home: SplashScreen(),
     );
   }
 }
